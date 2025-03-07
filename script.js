ebc7e2f0c5b023e940d746b99ea48da574d0c016
[Kacper Pudelko] <[kacper.pudelko.kpk@gmail.com]>
2025-03-07 16:49:40 +0100

    up

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
-const button = document.getElementById('button');
+
+gsap.to('.todo-app', { y: 200, duration: 1 });
 
 
 function addTask() {
@@ -10,7 +11,32 @@ function addTask() {
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
 
+    inputBox.value = '';
+    saveTasks();
 
 }
+
+listContainer.addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('checked');
+    } else if (event.target.tagName === 'SPAN') {
+        event.target.parentElement.remove();
+    }
+
+    saveTasks();
+}, false);
+
+function saveTasks() {
+    localStorage.setItem('tasks', listContainer.innerHTML);
+}
+
+function loadTasks() {
+    listContainer.innerHTML = localStorage.getItem('tasks');
+}
+
+loadTasks();
