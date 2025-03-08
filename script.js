553c14017710cdcdc11e5ece848748ec06d16251
[Kacper Pudelko] <[kacper.pudelko.kpk@gmail.com]>
2025-03-08 14:16:21 +0100

    animations

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
-gsap.to('.todo-app', { y: 200, duration: 1 });
+var tl = gsap.timeline({ default: { repeatDelay: 1, ease: 'power1.out' } });
 
-gsap.to('.todo-app-list', { y: 400, duration: 1 });
+tl.fromTo('.todo-app-header', { scale: 0, }, { scale: 1, ease: "elastic.out(1, 0.4)", duration: 2 });
+
+
+tl.fromTo('.todo-app-header h2', { opacity: 0 }, { opacity: 1, duration: 0.5 });
+tl.fromTo('.todo-app-header img ', { autoAlpha: 0 }, { autoAlpha: 1, duration: 0.5 });
+tl.fromTo('.todo-app-header', { y: 0 }, { y: -200, duration: 1.5 });
+
+tl.fromTo('.todo-app', { autoAlpha: 0, y: -200 }, { autoAlpha: 1, y: -200, duration: 1 });
+
+tl.fromTo(
+    ".todo-app-list",
+    { autoAlpha: 0, x: -200, y: -200 },
+    { autoAlpha: 1, x: 0, duration: 1 }
+);
+
+tl.fromTo('.todo-app-header img ', { y: 0 }, { y: -20, yoyo: true, repeat: -1, duration: 1, delay: 1.5 });
 
 
 
